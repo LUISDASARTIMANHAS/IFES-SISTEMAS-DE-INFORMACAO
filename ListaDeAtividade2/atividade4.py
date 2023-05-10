c9fb78e822978a79f3ab717b21283e3f26bb5f92
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-05-10 14:21:21 -0300

    Add files via upload

---
+++ ListaDeAtividade2/atividade4.py
@@ -0,0 +1,8 @@
+#Faça um programa que peça a hora atual e mostre uma mensagemna tela deacordocomos intervalos abaixo:
+hora = int(input("Insira As Horas.(14): "))
+
+#0 <= Hora < 5  “VAI DORMIR...” 
+# 5 <= Hora < 12  “BOM DIA” 
+# 12 <= Hora < 18  “BOA TARDE” 
+# 18 <= Hora < 24  “BOA NOITE” 
+# Outro valor  “HORA INVÁLIDA”
