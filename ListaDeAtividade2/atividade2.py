ae4d37a258d5a32741a94bbd4ff708dba04ed691
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-05-16 09:00:58 -0300

    Delete ListaDeAtividade2 directory

--- ListaDeAtividade2/atividade2.py
+++
@@ -1,9 +0,0 @@
-#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conformealetra,escrever: F ­ Feminino, M ­ Masculino ou Sexo Inválido
-abc = input("Insira seu sexo em letras (F,M) ")
-
-if(abc == "F") or (abc == "f"):
-    print("Sexo Indentificado como Feminino");
-elif(abc == "M") or (abc == "m"):
-    print("Sexo Indentificado como Masculino");
-else:
-    print("Sexo Invalido!");
