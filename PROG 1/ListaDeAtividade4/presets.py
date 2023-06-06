ad650fc27b8c1c1e1e40484785a9d7d375e057f0
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-06-06 14:42:07 -0300

    Add files via upload

--- PROG 1/ListaDeAtividade4/presets.py
+++ PROG 1/ListaDeAtividade4/presets.py
@@ -16,3 +16,40 @@ def validNota():
         print("Nota Invalido!")
         n = input("Insira uma Nota: ")
     return n
+
+def validNome():
+    nome = input("Insira seu nome: ");
+    nTam = len(nome)
+    while(nTam <= 3 ):
+        print("O nome não pode ser menor do que 3 caracteres");
+        nome = input("Insira seu nome: ");
+        nTam = len(nome)
+    return nome
+
+def validIdade():
+    idade = int(input("Insira sua idade: "));
+    while(idade <= 0) or (idade >= 50):
+        print("Idade invalida.");
+        idade = int(input("Insira sua idade: "));
+    return idade
+
+def validSalario():
+    salario = int(input("Insira seu salario: "));
+    while(salario < 0):
+        print("Salario invalido.")
+        salario = input("Insira Seu salario: ");
+    return salario
+
+def validSexo():
+    sx = input("Insira seu sexo(F/M): ").upper();
+    while (sx != "F") and (sx != "M"):
+        print("Sexo invalido.")
+        sx = input("Insira seu sexo(F/M): ").upper();
+    return sx
+
+def validUF():
+    uf = input("Insira seu Estado Civil: ").upper();
+    while (uf != "S") and (uf != "C") and (uf != "V") and (uf != "D"):
+        print("Sexo invalido.")
+        uf = input("Insira seu Estado Civil: ").upper();
+    return uf
