b4d734c451d4395c1ab1cb4e79df304d42d78008
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-07 16:36:48 -0300

    correção de bugs

--- PROG 1/TRABALHO-SISTEMA-DE-INVESTIMENTO/Index.py
+++ PROG 1/TRABALHO-SISTEMA-DE-INVESTIMENTO/Index.py
@@ -15,15 +15,11 @@ def main():
         elif op == 1:
             print("\n\nINSERIR\n\n")
             # Chamar a função para Inserir os dados nos vetores
-
-
         elif op == 2:
             print("\n\nPESQUISAR\n\n")
             pos = functions.pesq(DBCod)
             print(pos)
             # Imprimir os dados se encontrar
-
-
         elif op == 3:
             print("\n\nATUALIZAR\n\n")
             # Ler a informação para pesquisar
@@ -31,24 +27,15 @@ def main():
             # Ler os novos dados
             # Se encontrar, então atualizar o vetor, na posição pesquisada,
             #    com os novos dados
-
-
         elif op == 4:
             print("\n\nMAIOR\n\n")
             # Chamar a função para pesquisar no vetor o maior elemento
-
-
-
         elif op == 5:
             print("\n\nEXCLUIR\n\n")
             functions.pesqEDel(DBCod)
-
-
-        elif op == 6:
+        elif(op == 6):
             print("\n\nLISTAR\n\n")
             # Listar todos os dados dos vetores
-
-
         else:
             print("Opção inválida!")
 main()
