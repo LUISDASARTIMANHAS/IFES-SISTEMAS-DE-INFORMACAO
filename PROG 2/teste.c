6e7648eea8f5fc956d87c3a3afd51f73665037eb
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-08-10 17:27:13 -0300

    updated

--- PROG 2/teste.c
+++ PROG 2/teste.c
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <stdio.h>
-
-void main() {
-    int x = 10;
-    printf("Xtt = %d", x);
-=======
-#include <stdlib.h>
-#include <stdio.h>
-// #include <functions.h>
 
 int main() {
     int x;
@@ -18,5 +9,4 @@ int main() {
     
     printf("Xtt = %d", x);
     return 0;
->>>>>>> 76d2370daf677da9e274dbb8cc8afd5f2d79373a
 }
