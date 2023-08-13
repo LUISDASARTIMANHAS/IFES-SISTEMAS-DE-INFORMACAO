e33c58a1a62a9fffbbe58bdf59a8f0b25bad1b83
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-08-13 09:07:55 -0300

    updated

---
+++ PROG 2/listaDeAtividade1/atividade1.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// Uma P.A. (progressão aritmética) fica determinada pela sua razão (r) e pelo primeiro
+// termo(a1). Faça um programa em linguagem C, que seja capaz de determinar qualquer
+// termo de uma P.A., dada a razão, o primeiro termo e a quantidade de termos.
+// an = a1 + (n-1) x r
+
+int main(){
+int an;
+
+}
