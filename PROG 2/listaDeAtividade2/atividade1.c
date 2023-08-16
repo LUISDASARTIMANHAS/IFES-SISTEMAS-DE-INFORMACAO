c9f7de5fc8e80418932851698853865e967e8e58
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-08-16 14:36:53 -0300

    Add files via upload

--- PROG 2/listaDeAtividade2/atividade1.c
+++ PROG 2/listaDeAtividade2/atividade1.c
@@ -1,2 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "functions.c"
+
+// Construir um programa em C que leia dois números e efetue a adição. Caso o valor
+// somado seja maior que 20, este deverá ser apresentado somando-se a ele mais 8. Caso o
+// valor somado seja menor ou igual a 20, este deverá ser apresentado subtraindo-se 5.
+
+int main(){
+    copy();
+    return 0;
+}
+
+
+
+int somar(int num1, int num2){
+
+    return 0;
+}
+
+// int main(){
+//     int v, raizV;
+//     printf("Informe a raiz (v): ");
+//     scanf("%d", &v);
+    
+//     raizV = ;
+
+//     printf("A raiz de %d é: %d\n", v,raizV);
+//     return 0;
+// }
---
+++ PROG 2/listaDeAtividade2/functions.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int head(){
+    printf("\n ----------------------------------------------------");
+    printf("\n\t LUIS_DAS_ARTIMANHAS & PINGOBRAS S.A");
+    printf("\n\t Iniciando programa.....");
+    printf("\n----------------------------------------------------");
+    return 0;
+}
+
+
+int data(int dado1, int dado2,int dado3,int dado4){
+    printf("\n-----------------------DADOS--------------------");
+    printf("\n \t %s : %s", dado1);
+    printf("\n \t %s : %s", dado2);
+    printf("\n \t %s : %s", dado3);
+    printf("\n \t %s : %s", dado4);
+    printf("\n----------------------------------------------------");
+    return 0;
+}
+
+
+
+int copy(){
+    printf("\n----------------------------------------------------");
+    printf("\n\t DEVS:");
+    printf("\n\t LUIS_DAS_ARTIMANHAS.");
+    printf("\n\t PINGOBRAS S.A");
+    printf("\n----------------------------------------------------");
+    return 0;
+}
