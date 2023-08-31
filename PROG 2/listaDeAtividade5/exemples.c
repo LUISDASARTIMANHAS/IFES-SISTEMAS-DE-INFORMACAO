f06a4a9b68fd0890ac41eef2f7fac2e68820f8ff
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-08-31 13:38:22 -0300

    Add files via upload

---
+++ PROG 2/listaDeAtividade5/exemples.c
@@ -0,0 +1,12 @@
+#include "functions.c"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    head();
+    int tamMax = 1000;
+    int data[tamMax], data2[tamMax];
+
+    copy();
+    return 0;
+}
