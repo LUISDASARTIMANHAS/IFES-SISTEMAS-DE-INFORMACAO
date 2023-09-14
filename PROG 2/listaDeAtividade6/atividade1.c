fa84cf72cfcdec293e936cf4f666ae409e19b62a
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-09-14 13:26:17 -0300

    Add files via upload

---
+++ PROG 2/listaDeAtividade6/atividade1.c
@@ -0,0 +1,15 @@
+#include "functions.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    head();
+    int *data,maxSpace;
+    int elementos = 10;
+    alocarMEM(&data,&maxSpace);
+
+    
+    copy();
+    return 0;
+}
---
+++ PROG 2/listaDeAtividade6/atividade4.c
@@ -0,0 +1,15 @@
+#include "functions.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    head();
+    int *data,space;
+    int elementos = 10;
+    alocarMEM(&data,&space);
+
+    
+    copy();
+    return 0;
+}
