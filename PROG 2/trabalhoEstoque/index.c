d6a73d375629eb60aacf724824bd9864ed5fcfdc
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-10-28 12:51:03 -0300

    updated descriptions

--- PROG 2/trabalhoEstoque/index.c
+++ PROG 2/trabalhoEstoque/index.c
@@ -4,18 +4,8 @@
 #include "functions.c"
 #define MAXSPACE 1024
 
-// OBS: Reutilize a função pesquisar por código. Ela será utilizada pelas funções inserir, aumentar preço, aplicar desconto e excluir.
-// Para isso seu sistema deve possuir o seguinte menu:
-// SISTEMA DE ESTOQUE
-// 1 – Inserir produto
-// 2 – Pesquisar por código
-// 3 – Pesquisar por nome
-// 4 – Atualizar preço
-// 5 – Maior preço
-// 6 – Excluir
-// 7 - Listar
-// 0 – Sair
-// Escolha sua opção:
+// deve possuir as seguintes operações:
+//  Excluir: pedir o código do produto e excluir do vetor.
 int main() {
 	correct();
 	head();
@@ -65,8 +55,3 @@ int main() {
 	} while (op != 0);
 	return 0;
 }
-
-// deve possuir as seguintes operações:
-//  Atualizar preço: pedir o código do produto e a porcentagem de aumento ou
-// desconto. Pesquisar o código do produto no vetor e aplicar o aumento ou desconto no preço.
-//  Excluir: pedir o código do produto e excluir do vetor.
