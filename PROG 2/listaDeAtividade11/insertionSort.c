35cdf32e5031f4c4b337e0320df274ed73a1c92c
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-11-23 15:20:41 -0300

    updated

--- PROG 2/listaDeAtividade11/insertionSort.c
+++ PROG 2/listaDeAtividade11/insertionSort.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "functions.c"
-#define TAM 10
+#define TAM 5
 
 int main(){
     head();
     // int data3[TAM] = {642, 207, 162, 954, 156, 472, 829, 398, 992, 241};
-    int data2[TAM] = {41, 90, 4, 38, 67, 19, 57, 54, 44, 50};
+    int data2[TAM] = {3,7,1,1,2};
 
     insertionSort(data2,TAM);
 
--- PROG 2/listaDeAtividade11/shellsort.c
+++ PROG 2/listaDeAtividade11/shellsort.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "functions.c"
-#define TAM 10
+#define TAM 5
 
 int main(){
     head();
     // long int dataExtreme[1000] = {};
     // int data3[TAM] = {642, 207, 162, 954, 156, 472, 829, 398, 992, 241};
-    int data2[TAM] = {41, 90, 4, 38, 67, 19, 57, 54, 44, 50};
+    int data2[TAM] = {9,7,2,4,3,5,9,2,3};
 
     shellSort(data2,TAM);
     copy();
