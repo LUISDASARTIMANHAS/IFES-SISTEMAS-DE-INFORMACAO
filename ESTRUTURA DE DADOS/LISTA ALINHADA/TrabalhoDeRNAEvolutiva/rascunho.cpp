ef51afeb4b95901682939d4306401b433240256f
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-05-04 12:30:41 -0300

    updated

--- ESTRUTURA DE DADOS/LISTA ALINHADA/TrabalhoDeRNAEvolutiva/rascunho.cpp
+++ ESTRUTURA DE DADOS/LISTA ALINHADA/TrabalhoDeRNAEvolutiva/rascunho.cpp
@@ -1,12 +1,10 @@
 /*
-  Rede Neural Artificial Evolutiva (RNA-E)
-
-  Os pesos são atualizados a partir de um algoritmo
-  genético que busca minimizar os erros na fase de
-  treinamento.
+    Rede Neural Artificial Evolutiva (RNA-E)
+    Os pesos são atualizados a partir de um algoritmo genético que busca minimizar os erros na fase de treinamento.
 
 */
 
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -90,101 +88,115 @@ void poda(TLista *L);
 //===| Programa Principal |===========================
 int main()
 {
+    SetConsoleOutputCP(65001);
     inicializa(&lista);
     treinamento(&lista);
 }
 //===| Funções |======================================
-void inicializa(TLista *L)
-{
-    int i;
+FILE * abrirArquivo(char * nomeArq, char * modo) {
+    // ABRIR o arquivo
+    FILE * arq;
+    arq = fopen( nomeArq, modo );
+    if ( arq == NULL) {
+        printf("\n\n\t ERRO ao abrir o arquivo. \n\n");
+        return NULL;
+    }
+    return arq;
+}
 
-    L->licoes = NULL;
-    L->populacao = NULL;
-    L->individuoAtual = NULL;
-    L->totalIndividuos = 0;
-
-    for (i = 0; i < MAX_Entradas; i++)
-    {
-        L->entradas[i] = 0;
-    } // for
-
-    L->neuronios = NULL;
-    L->pesos = NULL;
-
-    printf("\t\t=====| REDE NEURAL ARTIFICIAL EVOLUTIVA |=====");
-    printf("\n\n\t\t=====| Configuracao da RNA |=====\n\n");
-    printf("\tInforme o TAMANHO da POPULACAO (em termos de individuos):\n");
-    printf("\t\tSugestao: 300 individuos.\n\t\tValor: ");
-    scanf("%d", &L->Qtd_Populacao);
-
-    geraIndividuos(L);
-
-    printf("\n\n\tInforme a QUANTIDADE de GERACOES maxima:");
-    printf("\n\tSugestao: 100 geracoes no total.\n\tValor: ");
-    scanf("%d", &L->Total_geracoes);
-
-    L->geracao_atual = 0;
-
-    printf("\n\n\tInforme o INTERVALO de GERACOES para a ocorrencia de MUTACOES:");
-    printf("\n\tSugestao: 5 (a cada 5 geracoes devem ocorrer mutacoes).\n\tValor: ");
-    scanf("%d", &L->Qtd_Geracoes_para_Mutacoes);
-
-    printf("\n\n\tInforme a QUANTIDADE de MUTACOES que devem ocorrer POR VEZ:");
-    printf("\n\tSugestao: 3 mutacoes por intervalo.\n\tValor: ");
-    scanf("%d", &L->Qtd_Mutacoes_por_vez);
-
-    printf("\n\nSINAPSE THRESHOLD (Limiar das Conexoes entre Neuronios):\n");
-    printf("Define a intensidade do sinal que sensibiliza cada neuronio.\n\n");
-    printf("\tInforme o SINAPSE THRESHOLD:\n\tSugestao: 0.60\n\tValor: ");
-    scanf("%f", &L->sinapseThreshold);
-
-    printf("\n\nLEARNING RATE (Taxa de Aprendizado): variacao dos pesos em cada ajuste (Aprendizado).\n");
-    printf("\n\tLEARNING RATE:\n\tSugestao: 0.20\n\tValor: ");
-    scanf("%f", &L->learningRate);
-
-    strcpy(L->objetivo, "Aprendizado da Funcao Logica P E Q");
-
-    printf("\n\n\tDefinindo as LICOES a serem aprendidas pela Rede Neural Artificial.\n\n");
-    geraLicoes(L);
-
-    printf("\n\n\tDefinindo os NEURONIOS que compoem a REDE NEURAL ARTIFICIAL.");
-    insereNeuronio(L, 1);
-    insereNeuronio(L, 2);
-    insereNeuronio(L, 3);
-    insereNeuronio(L, 4);
-    insereNeuronio(L, 5);
-
-    printf("\n\n\tEstabelecendo as CONEXOES (Sinapses) entre os NEURONIOS.");
-    estabelecendoSinapse(L, 1, 3, 0);
-    estabelecendoSinapse(L, 1, 4, 0);
-    estabelecendoSinapse(L, 2, 3, 0);
-    estabelecendoSinapse(L, 2, 4, 0);
-    estabelecendoSinapse(L, 3, 5, 1);
-    estabelecendoSinapse(L, 4, 5, 1);
-
-    L->fp = fopen("RNA_EVOLUTIVA_RELATORIO.TXT", "w");
-
-    fprintf(L->fp, "\n\t\t=====| REDE NEURAL ARTIFICIAL EVOLUTIVA |=====\n\n");
-    fprintf(L->fp, "\tOBJETIVO: %s.\n\n\tLicoes:\n", L->objetivo);
-    fprintf(L->fp, "\t LICAO    P    Q  (Resultado Esperado)\n");
-    fprintf(L->fp, "\t+------+----+----+---------------------+\n");
-
-    TLicao *licao = L->licoes;
-    int cont = 0;
-    while (licao != NULL)
-    {
-        fprintf(L->fp, "\t(%d) - %d   %d   %d\n", ++cont, licao->p, licao->q, licao->resultadoEsperado);
-        licao = licao->prox;
-    } // while
-
-    fprintf(L->fp, "\n\n");
-    fprintf(L->fp, "\tLearning Rate: %.2f\n", L->learningRate);
-    fprintf(L->fp, "\tSinapse Threshold: %.2f\n", L->sinapseThreshold);
-    fprintf(L->fp, "\tPopulacao MAXIMA: %d.\n", L->Qtd_Populacao);
-    fprintf(L->fp, "\t%d MUTACOES a cada sequencia de %d GERACOES.\n", L->Qtd_Mutacoes_por_vez, L->Qtd_Geracoes_para_Mutacoes);
-    fprintf(L->fp, "\tTOTAL de GERACOES: %d.\n\n\n", L->Total_geracoes);
-
-    printf("\n\n\tConfiguracao FINALIZADA!!!\n\n");
+void inicializa(TLista *L){
+	int i;
+	
+	L->licoes = NULL;
+	L->populacao = NULL;
+	L->individuoAtual = NULL;
+	L->totalIndividuos = 0;
+	
+	for(i=0; i < MAX_Entradas; i++){
+		L->entradas[i] = 0;
+	}//for
+	
+	L->neuronios = NULL;
+	L->pesos = NULL;
+	
+	printf("\t\t=====| REDE NEURAL ARTIFICIAL EVOLUTIVA |=====");
+	printf("\n\n\t\t=====| Configuracao da RNA |=====\n\n");
+	printf("\tInforme o TAMANHO da POPULACAO (em termos de individuos):\n");
+	printf("\t\tSugestao: 300 individuos.\n\t\tValor: ");
+	scanf("%d", &L->Qtd_Populacao);
+	
+	geraIndividuos(L);
+	
+	printf("\n\n\tInforme a QUANTIDADE de GERACOES maxima:");
+	printf("\n\tSugestao: 100 geracoes no total.\n\tValor: ");
+	scanf("%d", &L->Total_geracoes);
+	
+	L->geracao_atual = 0;
+	
+	printf("\n\n\tInforme o INTERVALO de GERACOES para a ocorrencia de MUTACOES:");
+	printf("\n\tSugestao: 5 (a cada 5 geracoes devem ocorrer mutacoes).\n\tValor: ");
+	scanf("%d", &L->Qtd_Geracoes_para_Mutacoes);
+	
+	printf("\n\n\tInforme a QUANTIDADE de MUTACOES que devem ocorrer POR VEZ:");
+	printf("\n\tSugestao: 3 mutacoes por intervalo.\n\tValor: ");
+	scanf("%d", &L->Qtd_Mutacoes_por_vez);
+	
+	printf("\n\nSINAPSE THRESHOLD (Limiar das Conexoes entre Neuronios):\n");
+	printf("Define a intensidade do sinal que sensibiliza cada neuronio.\n\n");
+	printf("\tInforme o SINAPSE THRESHOLD:\n\tSugestao: 0.60\n\tValor: ");
+	scanf("%f",&L->sinapseThreshold);
+	
+	printf("\n\nLEARNING RATE (Taxa de Aprendizado): variacao dos pesos em cada ajuste (Aprendizado).\n");
+	printf("\n\tLEARNING RATE:\n\tSugestao: 0.20\n\tValor: ");
+	scanf("%f",&L->learningRate);
+	
+	strcpy(L->objetivo,"Aprendizado da Funcao Logica P E Q");
+	
+	printf("\n\n\tDefinindo as LICOES a serem aprendidas pela Rede Neural Artificial.\n\n");
+	geraLicoes(L);
+	
+	printf("\n\n\tDefinindo os NEURONIOS que compoem a REDE NEURAL ARTIFICIAL.");
+	insereNeuronio(L,1);
+	insereNeuronio(L, 2);
+	insereNeuronio(L, 3);
+	insereNeuronio(L, 4);
+	insereNeuronio(L, 5);
+	
+	printf("\n\n\tEstabelecendo as CONEXOES (Sinapses) entre os NEURONIOS.");
+	estabelecendoSinapse(L,1,3,0);
+	estabelecendoSinapse(L,1,4,0);
+	estabelecendoSinapse(L,2,3,0);
+	estabelecendoSinapse(L,2,4,0);
+	estabelecendoSinapse(L,3,5, 1);
+	estabelecendoSinapse(L,4,5, 1);
+	
+	L->fp = abrirArquivo("data/RNA_EVOLUTIVA_RELATORIO.TXT", "w");
+    if(L->fp == NULL){
+        printf("Reabrindo arquivo no local da execucao");
+        L->fp = abrirArquivo("RNA_EVOLUTIVA_RELATORIO.TXT", "w");
+    }
+	
+	fprintf(L->fp,"\n\t\t=====| REDE NEURAL ARTIFICIAL EVOLUTIVA |=====\n\n");
+	fprintf(L->fp,"\tOBJETIVO: %s.\n\n\tLicoes:\n", L->objetivo);
+	fprintf(L->fp,"\t LICAO    P    Q  (Resultado Esperado)\n");
+	fprintf(L->fp,"\t+------+----+----+---------------------+\n");
+	
+	TLicao *licao = L->licoes;
+	int cont = 0;
+	while(licao != NULL){
+		fprintf(L->fp,"\t(%d) - %d   %d   %d\n", ++cont, licao->p, licao->q, licao->resultadoEsperado);
+		licao = licao->prox;
+	}//while
+	
+	fprintf(L->fp,"\n\n");
+	fprintf(L->fp,"\tLearning Rate: %.2f\n", L->learningRate);
+	fprintf(L->fp,"\tSinapse Threshold: %.2f\n", L->sinapseThreshold);
+	fprintf(L->fp,"\tPopulacao MAXIMA: %d.\n", L->Qtd_Populacao);
+	fprintf(L->fp,"\t%d MUTACOES a cada sequencia de %d GERACOES.\n", L->Qtd_Mutacoes_por_vez, L->Qtd_Geracoes_para_Mutacoes);
+	fprintf(L->fp,"\tTOTAL de GERACOES: %d.\n\n\n", L->Total_geracoes);
+	
+	printf("\n\n\tConfiguracao FINALIZADA!!!\n\n");
+	
 }
 //====================================================
 void geraIndividuos(TLista *L)
@@ -337,47 +349,46 @@ void printIndividuos(TLista *L) {
 }
 //=============================================================
 void treinamento(TLista *L){
-    printf("\n\n\t\t=====| INICIADO TREINAMENTO |=====\n\n");
-    fprintf(L->fp, "\n\n\tINICIO DO TREINAMENTO: ");
-    // ponteiro para a struct que armazena data e hora:
-    struct tm *data_hora_atual;
-    // variável do tipo time_t para armazenar o tempo em segundos.
-    time_t segundos;
-    // Obetendo o tempo em segundos.
-    time(&segundos);
-    // Para converter de segundos para o tempo local
-    // utilizamos a função localtime().
-    data_hora_atual = localtime(&segundos);
-
-    fprintf(L->fp, "Dia: %d", data_hora_atual->tm_mday);
-    fprintf(L->fp, "   Mes: %d", data_hora_atual->tm_mon);
-    fprintf(L->fp, "   Ano: %d\n\n", data_hora_atual->tm_year);
-
-    fprintf(L->fp, "Dia da Semana: %d.\n", data_hora_atual->tm_wday);
-
-    fprintf(L->fp, "%d", data_hora_atual->tm_hour);
-    fprintf(L->fp, ":%d", data_hora_atual->tm_min);
-    fprintf(L->fp, ":%d.\n\n", data_hora_atual->tm_sec);
-    descobreFimLista(L);
-    // int i;
-    // for (i = 0; i < L->Total_geracoes; i++)
-    // {
-    printf("etapa cruzamento\n");
-    cruzamento(L);
-    //printIndividuos(L);
-    //     if ((i % L->Qtd_Geracoes_para_Mutacoes) == 0){
-    //         printf("promovel mutacao\n");
-    //         promoveMutacoes(L);
-    //     } // if
-    printf("etapa da avaliacao de indivíduos\n");
-    avaliacaoIndividuos(L);
-    printIndividuos(L);
-    //     printf("etapa do ordenamento\n");
-    //     ordenamentoIndividuos(L);
-    //     printf("etapa da poda\n");
-    //     poda(L);
-    //     printf("Fim do primeiro loop\n");
-    // } // for
+	printf("\n\n\t\t=====| INICIADO TREINAMENTO |=====\n\n");
+	fprintf(L->fp,"\n\n\tINICIO DO TREINAMENTO: ");
+	//ponteiro para a struct que armazena data e hora:
+	struct tm *data_hora_atual;
+	//vari�vel do tipo time_t para armazenar o tempo em segundos.
+	time_t segundos;
+	//Obetendo o tempo em segundos.
+	time(&segundos);
+	//Para converter de segundos para o tempo local
+	//utilizamos a fun��o localtime().
+	data_hora_atual = localtime(&segundos);
+	
+	fprintf(L->fp,"Dia: %d", data_hora_atual->tm_mday);
+	fprintf(L->fp,"   Mes: %d", data_hora_atual->tm_mon+1);
+	fprintf(L->fp,"   Ano: %d\n\n", data_hora_atual->tm_year+1900);
+	
+	fprintf(L->fp,"Dia da Semana: %d.\n", data_hora_atual->tm_wday);
+	
+	fprintf(L->fp,"%d", data_hora_atual->tm_hour);
+	fprintf(L->fp,":%d", data_hora_atual->tm_min);
+	fprintf(L->fp,":%d.\n\n", data_hora_atual->tm_sec);
+	
+	int i;
+	for(i= 0; i < L->Total_geracoes; i++){
+		cruzamento(L);
+		
+		if((i % L->Qtd_Geracoes_para_Mutacoes) == 0){
+			promoveMutacoes(L);
+		}//if
+		
+		avaliacaoIndividuos(L);
+		
+		ordenamentoIndividuos(L);
+		
+		poda(L);
+		
+	}//for
+	printf("Salvando dados...");
+	fclose(L->fp);
+	printf("Salvo com sucesso!");
 }
 //=============================================================
 void insere(TLista *lista, TIndividuo *filho){
