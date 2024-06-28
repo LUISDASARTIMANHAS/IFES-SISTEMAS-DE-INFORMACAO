1dcf317bc83ad497441fbaee013c8eb250ada638
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-06-28 13:38:01 -0300

    UPDATED

---
+++ OAC/OpenMP Trabalho distribuicao de threads/index.py
@@ -0,0 +1,10 @@
+import time
+
+def valor_com_imposto(qtde):
+    for i in range(qtde):
+        novo_valor = qtde * 1.1
+
+inicio = time.time()
+valor_com_imposto(1000000000)
+fim = time.time()
+print(fim - inicio)
