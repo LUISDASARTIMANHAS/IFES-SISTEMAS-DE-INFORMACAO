57397c69d9308a1694047733d77a4b878d7842c8
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-07-16 15:36:14 -0300

    Update debug.cmd

--- TEMPLATES/TRABALHOS/debug.cmd
+++ TEMPLATES/TRABALHOS/debug.cmd
@@ -1,4 +1,5 @@
+set fileName=pilhaComplexa
 if EXIST "debug.exe" (
    del "debug.exe"
 ) 
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ pilhaComplexa.cpp -o "/debug.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ %fileName%.cpp -o "/debug.exe"
