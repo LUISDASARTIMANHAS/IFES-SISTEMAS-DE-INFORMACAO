1306f59109db2374ef238e48a1c8815d6337beab
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-15 14:30:29 -0300

    updated

---
+++ TPA/index.c
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <headers.h>
+// #include "functions.c"
+
+//===| Programa Principal |===========================
+int main(){
+    insereArvBin();
+    
+    return 0;
+}
