1927ad529dfcc3056727ca86d0f89a4f7c0c096a
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-23 20:41:28 -0300

    latest updated

--- TPA/ListaEncSimples/compile.cmd
+++ TPA/ListaEncSimples/compile.cmd
@@ -1,18 +1,18 @@
 @ECHO OFF
 set /p versao=Insira a versao: V
-set fileName=TrabalhoArvoreBinAVL
+set fileName=listaEncadeadaSimples
 set fullFileName=%fileName%.V%versao%
 
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ rascunho.cpp -o "builds/%fileName%.V%versao%.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ rascunho.c -o "builds/rascunho.V%versao%.exe"
 
 if exist "builds/debug.exe" (
    del "builds/debug.exe"
 )
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ index.cpp -o "builds/debug.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ listaEncadeadaSimples.c -o "builds/debug.exe"
 
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ index.cpp -o "builds/%fullFileName%.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ listaEncadeadaSimples.c -o "builds/%fullFileName%.exe"
 
-tar -a -c -f "zip/%fullFileName%.zip" *data *builds *.cpp functions.c *.cmd
+tar -a -c -f "zip/%fullFileName%.zip" *data *builds *.cpp *.c *.cmd
 
 msg * /v /w %fullFileName%.exe foi compilado!
 
