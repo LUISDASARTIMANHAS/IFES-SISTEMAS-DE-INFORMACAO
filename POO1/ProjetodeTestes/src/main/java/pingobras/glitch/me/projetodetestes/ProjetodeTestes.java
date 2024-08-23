78313c253b9083e9b867b0bd0cd3e194ce280558
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-23 10:51:58 -0300

    updated

---
+++ POO1/ProjetodeTestes/src/main/java/pingobras/glitch/me/projetodetestes/ProjetodeTestes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pingobras.glitch.me.projetodetestes;
+
+/**
+ *
+ * @author LUIS DAS ARTIMANHAS
+ */
+public class ProjetodeTestes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
