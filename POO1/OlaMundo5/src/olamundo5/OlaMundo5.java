1be654d73d5a28d3012d8944116e7b8f1b0a42f1
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-27 06:13:54 -0300

    updated

---
+++ POO1/OlaMundo5/src/olamundo5/OlaMundo5.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olamundo5;
+
+/**
+ *
+ * @author LUIS DAS ARTIMANHAS
+ */
+public class OlaMundo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        int n1 = 10;
+        int n2 = 20;
+        int n3 = n1 + n2;
+        
+        System.out.println("O valor de N3 é: " + n3);
+        
+        
+        
+    }
+    
+}
