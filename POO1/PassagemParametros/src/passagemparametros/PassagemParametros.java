1932226e5a9b81e5c6daca8b049f2dd7cb1602bf
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-29 15:31:08 -0300

    updated

---
+++ POO1/PassagemParametros/src/passagemparametros/PassagemParametros.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package passagemparametros;
+
+public class PassagemParametros {
+
+    public static void main(String[] args) {
+        System.out.println("O tamanho do array é:" + args.length);
+
+        System.out.println(args[2] + ". " + args[0] + " " + args[1]);
+    }
+ 
+}
