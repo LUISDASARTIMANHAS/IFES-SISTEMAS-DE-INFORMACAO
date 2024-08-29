ac23d1fffe7615e0fdd861729360255f97d297ed
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-29 16:28:53 -0300

    updated

--- POO1/PassagemParametros/src/passagemparametros/PassagemParametros.java
+++ POO1/PassagemParametros/src/passagemparametros/PassagemParametros.java
@@ -10,6 +10,14 @@ public class PassagemParametros {
         System.out.println("O tamanho do array é:" + args.length);
 
         System.out.println(args[2] + ". " + args[0] + " " + args[1]);
+
+        double nota1, nota2, trabalho1, trabalho2, media;
+        nota1 = Double.parseDouble(args[0]);
+        nota2 = Double.parseDouble(args[1]);
+        trabalho1 = Double.parseDouble(args[2]);
+        trabalho2 = Double.parseDouble(args[3]);
+        media = (nota1 + nota2 + trabalho1 + trabalho2) / 4;
+        System.out.println("Média = " + media);
     }
- 
+
 }
