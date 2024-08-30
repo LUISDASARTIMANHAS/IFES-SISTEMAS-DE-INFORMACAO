8d58c1999124d76330cee90e2560841d8e87d043
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-08-30 14:25:22 -0300

    updated trabalho TabelaHash

---
+++ TPA/TabelaHashUtilizandoenderecamentoFechado/TabelaHash.cpp
@@ -0,0 +1,15 @@
+// Produzido por LUCAS GARCIA & LUIS AUGUSTO DE SOUZA
+// chave e a matricula (K)
+// mod e o resto da divisão
+// M e o tamanho do vetor
+#include <TabelaHash.h>
+void inicializa(){
+	printf("\n\n\t\t=====| Tabela Hash |=====\n");
+    
+}
+
+//===| Programa Principal |===========================
+int main(){
+	SetConsoleOutputCP(65001);
+	inicializa();
+}
---
+++ TPA/TabelaHashUtilizandoenderecamentoFechado/debug.cmd
@@ -0,0 +1,5 @@
+set fileName=pilhaComplexa
+if EXIST "debug.exe" (
+   del "debug.exe"
+) 
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ %fileName%.cpp -o "/debug.exe"
