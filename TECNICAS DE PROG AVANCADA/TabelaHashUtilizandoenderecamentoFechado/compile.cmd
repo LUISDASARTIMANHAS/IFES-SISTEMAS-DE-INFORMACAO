b8e4bce99a12fc9c067239853ad1ff4b767dba67
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-09-04 13:43:39 -0300

    updated

--- TECNICAS DE PROG AVANCADA/TabelaHashUtilizandoenderecamentoFechado/compile.cmd
+++ TECNICAS DE PROG AVANCADA/TabelaHashUtilizandoenderecamentoFechado/compile.cmd
@@ -3,14 +3,14 @@ set /p versao=Insira a versao: V
 set fileName=tabelaHash
 set fullFileName=%fileName%.V%versao%
 
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ rascunho.cpp -o "builds/%fileName%.V%versao%.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ rascunho.c -o "builds/%fileName%.V%versao%.exe"
 
 if exist "builds/debug.exe" (
    del "builds/debug.exe"
 )
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ index.cpp -o "builds/debug.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ tabelaHash.c -o "builds/debug.exe"
 
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ index.cpp -o "builds/%fullFileName%.exe"
+g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ tabelaHash.c -o "builds/%fullFileName%.exe"
 
 tar -a -c -f "zip/%fullFileName%.zip" *data *builds *.h *.cpp functions.c *.cmd
 
