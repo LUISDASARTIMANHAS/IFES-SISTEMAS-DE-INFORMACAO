f0ec5259bc0e46fef995cc1babeddd07fb5a4210
Lucas Garcia <111055697+LucasGarkDev@users.noreply.github.com>
2024-09-05 13:43:37 -0300

    Update projetoTabela.h

--- TECNICAS DE PROG AVANCADA/TabelaHashUtilizandoenderecamentoFechado/rascunho/projetoTabela.h
+++ TECNICAS DE PROG AVANCADA/TabelaHashUtilizandoenderecamentoFechado/rascunho/projetoTabela.h
@@ -10,10 +10,8 @@
 #define INICIO "------------INICIO------------" 
 #define RESULTADO "------------RESULTADO------------"
 #define CORTE "------------------------"
-
-typedef char string[101];
 typedef struct tipoElemento{
-    int valor;                     
+    long int valor;                     
     char nome[100];                
     struct tipoElemento *prox;     
 } TElemento;
@@ -24,11 +22,11 @@ typedef struct tipoLista{
 }TLista;
 
 typedef struct tipoTabelaHash {
-    TLista *vetorListas;
-    int tamanho;
+    TLista *vetorListas;  
+    int tamanho;          
 }TabelaHash;
 
-int funcaoHash(int matricula, int tamanho);
+int funcaoHash(long int matricula, int tamanho);
 int contarMatriculas(FILE *arquivoLista);
 int contarTotalMatriculas(TabelaHash *tabela);
 int ehPrimo(int num);
@@ -37,9 +35,9 @@ void inicializarTabelaHash(TabelaHash *tabela, int tamanho);
 void inicializarTabela(TabelaHash *tabelaHash, FILE *arquivoLista);
 void lerEInserirMatriculas(TabelaHash *tabelaHash, FILE *arquivoLista);
 void executarMenu(TabelaHash *tabelaHash);
-int pesquisarTabelaHash(TabelaHash *tabela, int matricula);
-void inserirTabelaHash(TabelaHash *tabela, int matricula, char *nome);
-void excluirTabelaHash(TabelaHash *tabela, int matricula);
+int pesquisarTabelaHash(TabelaHash *tabela, long int matricula);
+void inserirTabelaHash(TabelaHash *tabela, long int matricula, char *nome);
+void excluirTabelaHash(TabelaHash *tabela, long int matricula);
 void exibeTabelaHash(TabelaHash *tabela);
 void liberarTabelaHash(TabelaHash *tabela);
 FILE * abrirArquivo(char * nomeArq, char * modo);
@@ -47,12 +45,12 @@ void construirListaDoZero(TLista *lista);
 void lerArquivo(TLista *lista, FILE *arquivoLista);
 void inicializa(TLista *lista, FILE *arquivoLista);
 void gravarListaEmArquivo(TLista *lista, FILE *arquivoLista);
-int pesquisarMatricula2(TLista *lista, int matriculaBusca); 
+int pesquisarMatricula2(TLista *lista, long int matriculaBusca); 
 int pesquisarMatricula(TLista *lista);
-void inserir(TLista *lista, int valor, char *nome);
+void inserir(TLista *lista, long int valor, char *nome);
 int pedirOpcao3();
 void exibeLista(TLista lista);
-void excluirLista(TLista *lista, int valor);
+void excluirLista(TLista *lista, long int valor);
 int pedirOpcao();
 int pedirNum(int caminhoASerEscolhido);
 void imprimirTabelaHash(TabelaHash *tabela);
