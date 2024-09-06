cd9515c162efc197637c119b59e90f3ad065f6f9
Lucas Garcia <111055697+LucasGarkDev@users.noreply.github.com>
2024-09-06 13:35:10 -0300

    Update projetoTabela.h

--- TECNICAS DE PROG AVANCADA/TabelaHashUtilizandoenderecamentoFechado/rascunho/projetoTabela.h
+++ TECNICAS DE PROG AVANCADA/TabelaHashUtilizandoenderecamentoFechado/rascunho/projetoTabela.h
@@ -1,19 +1,9 @@
-#include <windows.h>
-#define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #include <time.h>
-// Defina constantes para as sequências de escape ANSI das cores
-#define RED "\x1b[31m"
-#define BLUE "\x1b[34m"
-#define GREEN "\x1b[32m"
-#define YELLOW "\x1b[33m"
-#define RESET "\x1b[0m"
-
-#define SEPARETOR BLUE "\n================================================\n"  RESET
 
 typedef char string[101];
 
