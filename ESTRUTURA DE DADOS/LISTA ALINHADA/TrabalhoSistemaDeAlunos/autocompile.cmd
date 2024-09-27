967f8fbd25c0b31e1eb6c210b7d8449423dc87b8
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-09-26 22:32:54 -0300

    Delete ESTRUTURA DE DADOS/LISTA ALINHADA/TrabalhoSistemaDeAlunos directory

--- ESTRUTURA DE DADOS/LISTA ALINHADA/TrabalhoSistemaDeAlunos/autocompile.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-:autocompile
-	cls
-	%*
-	timeout /t 45 > NUL
-	g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ index.cpp -o builds/trabalhoSistemaDeAlunos.V48-autoCompile.exe
-goto autocompile
--- ESTRUTURA DE DADOS/LISTA ALINHADA/TrabalhoSistemaDeAlunos/compile.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-set /p versao=Insira a versao: 
-g++ -Wall -g3 -Wextra -static -static-libgcc -static-libstdc++ index.cpp -o builds/trabalhoSistemaDeAlunos.V%versao%.exe
-msg * /v /w trabalhoSistemaDeAlunos.V%versao%.exe foi compilado!
-start builds/trabalhoSistemaDeAlunos.V%versao%.exe
