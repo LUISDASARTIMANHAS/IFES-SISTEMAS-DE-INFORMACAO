282abc65894eca42c760ce35fd4e1f93887be9a9
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-10-07 14:30:19 -0300

    Interfaces

---
+++ POO1/Interfaces/src/interfaces/Atleta.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaces;
+
+public interface Atleta {
+
+    public static final int i = 0;
+
+    public abstract void aquecer();
+}
