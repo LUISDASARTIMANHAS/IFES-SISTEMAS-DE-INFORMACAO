deeae8bc9ec81f5023421d0b585ed334075ab419
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-03-24 16:10:08 -0300

    add pagina de exemplo do seminario de css

---
+++ DesWeb1/Seminario de CSS/src/js/index.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll("input");
+const box = document.getElementById("box");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    alterarUnidade(input.value);
+  });
+});
+
+function alterarUnidade(value) {
+  if (value == "vh") {
+    box.style.height = "100" + value;
+  } else {
+    box.style.width = "100" + value;
+    box.textContent = `Unidade Atual: 100${value}`;
+    box.style.height = "100px";
+  }
+}
