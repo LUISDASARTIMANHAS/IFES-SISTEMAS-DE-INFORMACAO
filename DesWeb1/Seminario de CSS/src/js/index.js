d3c517a3b7e5ec2da7806f8854ffef878f8b79d9
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-03-28 16:27:49 -0300

    updated

--- DesWeb1/Seminario de CSS/src/js/index.js
+++ DesWeb1/Seminario de CSS/src/js/index.js
@@ -9,10 +9,17 @@ inputs.forEach((input) => {
 
 function alterarUnidade(value) {
   if (value == "vh") {
-    box.style.height = "100" + value;
-  } else {
+    box.style.height = `100${value}`;
+    box.style.height = `Unidade Atual: ${box.style.height}`;
+  } else if(value == "ex"){
+    box.style.fontSize = `7${value}`;
+    box.textContent = `Unidade Atual: ${box.style.fontSize}`;
+    box.style.width = "95%";
+  }
+  else {
     box.style.width = "100" + value;
     box.textContent = `Unidade Atual: ${box.style.width}`;
     box.style.height = "100px";
+    box.style.fontSize = `16px`;
   }
 }
