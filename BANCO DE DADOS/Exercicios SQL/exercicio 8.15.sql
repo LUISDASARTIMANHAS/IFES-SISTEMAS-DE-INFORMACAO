73a53e4f9c92bd1d071e9f0b6230d4f1df7be534
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-06-17 19:51:11 -0300

    updated

--- BANCO DE DADOS/Exercicios SQL/exercicio 8.15.sql
+++ BANCO DE DADOS/Exercicios SQL/exercicio 8.15.sql
@@ -3,16 +3,9 @@
 SELECT 
     c.cod_cliente,
     c.nom_cliente,
-    f.nom_filme,
-	count(c.cod_cliente)
+    COUNT(l.cod_fita) AS qtd_fitas_alugadas
 FROM cliente c
-LEFT JOIN locacao l
-	ON c.cod_cliente = l.cod_cliente
-LEFT JOIN fita ft 
-	ON l.cod_fita = ft.cod_fita
-LEFT JOIN filme f 
-	ON ft.cod_filme = f.cod_filme
-group by
-	c.cod_cliente,
-	f.nom_filme
+LEFT JOIN locacao l ON c.cod_cliente = l.cod_cliente
+GROUP BY c.cod_cliente, c.nom_cliente
 ORDER BY c.cod_cliente;
+
--- BANCO DE DADOS/Exercicios SQL/exercicio 8.4.sql
+++ BANCO DE DADOS/Exercicios SQL/exercicio 8.4.sql
@@ -1,14 +1,13 @@
--- 8.4. Exiba o nome dos filmes, a descrição de seu gênero, 
--- mas somente para as fitas que estão disponíveis (sit_fita =2) 
+-- ALTERADA 
+-- Exiba o nome e código das fitas que possuem situação de locadas (1) e que possuem gênero 6
+-- ou 8.
+
 select distinct 
-		filme.nom_filme
-,		genero.nom_genero
+		fil.nom_filme
+,		fit.cod_fita
 , 		sit_fita
-from filme
-JOIN genero
-	ON genero.cod_genero = filme.cod_genero
-join fita
-	on fita.cod_filme = filme.cod_filme
-	and fita.sit_fita = 1
-;
--- select distinct sit_fita from fita;
+from filme fil
+JOIN fita fit
+	ON fil.cod_filme = fit.cod_filme
+and fit.sit_fita = 1
+and fil.cod_genero in (6,8) 
