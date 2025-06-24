f8c39e1df228174a4c6ea20e063dad690447e05a
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-06-24 18:26:20 -0300

    fixed bugs

--- BANCO DE DADOS/Trabalho de Banco de Dados/inserir 2 banco.sql
+++ BANCO DE DADOS/Trabalho de Banco de Dados/inserir 2 banco.sql
@@ -6,13 +6,6 @@ INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (7, 
 INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (8, 8, '1g', '1 vez ao dia');
 INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (9, 9, '20mg', '1 vez ao dia');
 INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (10, 10, '250mg', '3 vezes ao dia');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (11, 1, '500mg', '8/8 horas');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (12, 2, '400mg', '12/12 horas');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (13, 3, '500mg', '8/8 horas');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (14, 4, '500mg', '6/6 horas');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (15, 5, '20mg', '1x ao dia');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (16, 6, '10mg', '1x ao dia');
-INSERT INTO Receita (idConsulta, idMedicamento, dose, intervaloDose) VALUES (17, 7, '50mg', '1x ao dia');
 
 
 -- Inserindo mais associações de planos e pacientes
