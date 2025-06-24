82c1a2b0931e59ac819ffe7d8b6275e3394c2458
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-06-24 13:37:03 -0300

    sync

--- DesWeb1/atividade-bootstrap-formulario/js/script.js
+++ DesWeb1/atividade-bootstrap-formulario/js/script.js
@@ -1,2 +1,32 @@
 // Aqui você pode adicionar futura validação ou interações JS
 console.log("Formulário carregado com sucesso.");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const celular = document.getElementById("celular");
+  const telefone = document.getElementById("telefone");
+  const cep = document.getElementById("cep");
+
+  function aplicarMascara(input, mascara) {
+    input.addEventListener("input", () => {
+      let i = input.value.replace(/\D/g, "");
+      let m = mascara;
+      let resultado = "";
+      let j = 0;
+
+      for (let k = 0; k < m.length && j < i.length; k++) {
+        if (m[k] === "X") {
+          resultado += i[j++];
+        } else {
+          resultado += m[k];
+        }
+      }
+
+      input.value = resultado;
+    });
+  }
+
+  aplicarMascara(celular, "(XX) 9XXXX-XXXX");
+  aplicarMascara(telefone, "(XX) XXXX-XXXX");
+  aplicarMascara(cep, "XXXXX-XXX");
+});
