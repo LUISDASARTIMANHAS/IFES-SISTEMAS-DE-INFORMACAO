a9bf364538c03770eb3b2da3d1543f53d3db4eb0
COL-10038-LI03 <luisaugustodesouza785@gmail.com>
2026-09-22 20:21:13 -0300

    updated malha+

--- Dispositivos Moveis/malhamais/android/app/build.gradle.kts
+++ Dispositivos Moveis/malhamais/android/app/build.gradle.kts
@@ -3,6 +3,21 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
